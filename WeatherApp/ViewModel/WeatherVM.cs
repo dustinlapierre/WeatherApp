47fbad1297f7bf94233057bd47ce32965a2389ed
Dustin Lapierre <dustinlapierre@yahoo.com>
2022-06-11 13:24:32 +0200

    Added new property of type ObservableCollection to hold the possible cities based on a search

--- WeatherApp/ViewModel/WeatherVM.cs
+++ WeatherApp/ViewModel/WeatherVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace WeatherApp.ViewModel
                 OnPropertyChanged("Query");
             }
         }
+
+        public ObservableCollection<City> Cities { get; set; }
 
         private CurrentConditions currentConditions;
 
@@ -53,6 +56,10 @@ namespace WeatherApp.ViewModel
         public async void MakeQuery()
         {
             var cities = await AccuWeatherHelper.GetCities(Query);
+
+            Cities.Clear();
+            foreach (City city in cities)
+                Cities.Add(city);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -60,6 +67,7 @@ namespace WeatherApp.ViewModel
         public WeatherVM()
         {
             SearchCommand = new SearchCommand(this);
+            Cities = new ObservableCollection<City>();
         }
 
         private void OnPropertyChanged(string propertyName)
